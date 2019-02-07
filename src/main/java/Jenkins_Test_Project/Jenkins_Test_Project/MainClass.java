41f8fccf15d373d77233815d96a5f99cd12e775e
hiwotbishaw <hiwotbishaw@gmail.com>
2019-02-06 23:08:09 -0600

    created ui

---
+++ src/main/java/Jenkins_Test_Project/Jenkins_Test_Project/MainClass.java
@@ -0,0 +1,48 @@
+package Jenkins_Test_Project.Jenkins_Test_Project;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+
+public class MainClass {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainClass window = new MainClass();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainClass() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblWelcome = new JLabel("Welcome");
+		frame.getContentPane().add(lblWelcome, BorderLayout.CENTER);
+	}
+
+}
