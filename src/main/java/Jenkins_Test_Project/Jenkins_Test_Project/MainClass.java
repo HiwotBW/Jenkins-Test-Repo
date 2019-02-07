0572398378e7d4a3297418d46468fe4eac88a062
hiwotbishaw <hiwotbishaw@gmail.com>
2019-02-06 23:17:24 -0600

    added new ui and set test to fail

--- src/main/java/Jenkins_Test_Project/Jenkins_Test_Project/MainClass.java
+++ src/main/java/Jenkins_Test_Project/Jenkins_Test_Project/MainClass.java
@@ -5,6 +5,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JPanel;
+import java.awt.Font;
 
 public class MainClass {
 
@@ -40,9 +43,17 @@ public class MainClass {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		JLabel lblWelcome = new JLabel("Welcome");
-		frame.getContentPane().add(lblWelcome, BorderLayout.CENTER);
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 434, 261);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblWelcome = new JLabel("      Welcome");
+		lblWelcome.setFont(new Font("Footlight MT Light", Font.PLAIN, 40));
+		lblWelcome.setBounds(96, 98, 262, 58);
+		panel.add(lblWelcome);
+		lblWelcome.setForeground(Color.RED);
 	}
-
 }
