9b0baf8c879b8139e71c1f5fae713c855e168069
hiwotbishaw <hiwotbishaw@gmail.com>
2019-02-06 22:09:30 -0600

    working maven project

---
+++ src/main/java/Jenkins_Test_Project/Jenkins_Test_Project/App.java
@@ -0,0 +1,13 @@
+package Jenkins_Test_Project.Jenkins_Test_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
